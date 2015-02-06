62261d6a4527c9247a50bfc85bfcbc43695f5f3c
TechStack <techStackLp@gmail.com>
2015-02-06 16:05:19 -0500

    added 3d model for possible future machine

--- src/main/java/com/projectreddog/machinemod/model/ModelExcavator.java
+++ src/main/java/com/projectreddog/machinemod/model/ModelExcavator.java
@@ -48,6 +48,7 @@ public class ModelExcavator extends ModelTransportable {
 				GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
 				GL11.glEnable(GL11.GL_BLEND);
 				GL11.glColor4d(1, 1, 1, .50d);
+
 			}
 		}
 		this.renderGroupObject("body_Cube");
