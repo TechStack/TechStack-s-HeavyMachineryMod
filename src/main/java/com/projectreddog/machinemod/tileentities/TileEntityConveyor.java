4833b420a7c6a1a7926d6a8bc1a404bbc1870102
TechStack <techStackLp@gmail.com>
2015-02-20 20:52:52 -0500

    Moving entities Vert on vert conveyors
    Needs works on all directions

--- src/main/java/com/projectreddog/machinemod/tileentities/TileEntityConveyor.java
+++ src/main/java/com/projectreddog/machinemod/tileentities/TileEntityConveyor.java
@@ -8,9 +8,12 @@ import net.minecraft.entity.item.EntityItem;
 import net.minecraft.server.gui.IUpdatePlayerListBox;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.AxisAlignedBB;
+import net.minecraft.util.BlockPos;
 import net.minecraft.util.EnumFacing;
 
 import com.projectreddog.machinemod.block.BlockMachineModConveyor;
+import com.projectreddog.machinemod.init.ModBlocks;
+import com.projectreddog.machinemod.utility.LogHelper;
 
 public class TileEntityConveyor extends TileEntity implements IUpdatePlayerListBox {
 
@@ -36,12 +39,35 @@ public class TileEntityConveyor extends TileEntity implements IUpdatePlayerListB
 		// rotated.
 
 		if (!worldObj.isBlockPowered(this.pos)) {
-			boundingBox = new AxisAlignedBB(this.pos.offsetUp(), this.pos.offsetUp().add(1, 1, 1));
-			List list = worldObj.getEntitiesWithinAABB(EntityItem.class, boundingBox);
-			processEntitiesInList(list);
-			boundingBox = new AxisAlignedBB(this.pos.offsetUp(), this.pos.offsetUp().add(1, 1, 1));
-			list = worldObj.getEntitiesWithinAABB(EntityLivingBase.class, boundingBox);
-			processEntitiesInList(list);
+
+			if (worldObj.getBlockState(pos).getBlock() == ModBlocks.machineconveyor) {
+				if (BlockMachineModConveyor.shouldLift(worldObj, this.pos)) {
+					EnumFacing checkDirection = (EnumFacing) worldObj.getBlockState(this.pos).getValue(BlockMachineModConveyor.FACING);
+					BlockPos bp = this.pos.offset(checkDirection.getOpposite());
+					double xOffset = 0, zOffset = 0;
+					if (checkDirection == EnumFacing.EAST) {
+						bp = bp.offsetWest();
+					} else if (checkDirection == EnumFacing.WEST) {
+						bp = bp.offsetEast();
+					} else if (checkDirection == EnumFacing.NORTH) {
+						bp = bp.offsetSouth();
+
+					} else if (checkDirection == EnumFacing.SOUTH) {
+						bp = bp.offsetNorth();
+					}
+
+					boundingBox = new AxisAlignedBB(bp, this.pos.offsetUp().add(1, 1, 1));
+
+					LogHelper.info("Block at" + this.pos + "pos1" + bp + "POS2" + this.pos.offsetUp().add(1, 1, 1) + "EF" + checkDirection);
+				} else {
+					boundingBox = new AxisAlignedBB(this.pos.offsetUp(), this.pos.offsetUp().add(1, 1, 1));
+				}
+				List list = worldObj.getEntitiesWithinAABB(EntityItem.class, boundingBox);
+				processEntitiesInList(list);
+
+				list = worldObj.getEntitiesWithinAABB(EntityLivingBase.class, boundingBox);
+				processEntitiesInList(list);
+			}
 		}
 
 	}
@@ -50,6 +76,8 @@ public class TileEntityConveyor extends TileEntity implements IUpdatePlayerListB
 		for (int i = 0; i < par1List.size(); ++i) {
 			Entity entity = (Entity) par1List.get(i);
 			if (entity != null) {
+				LogHelper.info("ent in bounds");
+
 				// if (entity instanceof EntityLivingBase) {
 				// ((EntityLivingBase) entity).moveEntity(.1d, 0, 0);
 				// } else {
@@ -73,11 +101,28 @@ public class TileEntityConveyor extends TileEntity implements IUpdatePlayerListB
 					z = 0;
 				}
 
+				if (BlockMachineModConveyor.shouldLift(worldObj, this.pos)) {
+					// Should lift maybe?
+					if (new BlockPos(entity.posX, entity.posY, entity.posZ).getX() == this.pos.getX() && new BlockPos(entity.posX, entity.posY, entity.posZ).getZ() == this.pos.getZ()) {
+						// entity in same pos no Y adustment
+						LogHelper.info("ent Y" + entity.posY + " bock pos " + this.pos.getY());
+
+					} else {
+
+						if (entity.posY < this.pos.getY() + 1) {
+							LogHelper.info("ent Y" + entity.posY + " bock pos " + this.pos.getY());
+							entity.fallDistance = 0; // set so no damage to players going up long virt shafts
+							entity.motionY = 0;
+							y = MoveSpeed;
+						}
+					}
+
+				}
+
 				entity.moveEntity(x, y, z);
 				// entity.setPosition(entity.getPosition().getX() + 0.1d, entity.getPosition().getY(), entity.getPosition().getZ());
 				// }
 			}
 		}
 	}
-
 }
