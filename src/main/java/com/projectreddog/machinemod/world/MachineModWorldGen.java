30f5ab138e51cf7b94ebecca3f9a3d2ea558c8b4
TechStack <techStackLp@gmail.com>
2015-12-17 22:18:33 -0500

    Starting world gen

---
+++ src/main/java/com/projectreddog/machinemod/world/MachineModWorldGen.java
@@ -0,0 +1,50 @@
+package com.projectreddog.machinemod.world;
+
+import java.util.Random;
+
+import com.projectreddog.machinemod.init.ModBlocks;
+
+import net.minecraft.util.BlockPos;
+import net.minecraft.world.World;
+import net.minecraft.world.chunk.IChunkProvider;
+import net.minecraft.world.gen.feature.WorldGenMinable;
+import net.minecraft.world.gen.feature.WorldGenerator;
+import net.minecraftforge.fml.common.IWorldGenerator;
+
+public class MachineModWorldGen implements IWorldGenerator {
+
+	private WorldGenerator gen_machineMod_ore;
+
+	public MachineModWorldGen() {
+		this.gen_machineMod_ore = new WorldGenMinable(ModBlocks.machineasphalt.getDefaultState(), 8);
+	}
+
+	@Override
+	public void generate(Random random, int chunkX, int chunkZ, World world, IChunkProvider chunkGenerator, IChunkProvider chunkProvider) {
+		switch (world.provider.getDimensionId()) {
+		case 0: // Overworld
+
+			break;
+		case -1: // Nether
+
+			break;
+		case 1: // End
+
+			break;
+		}
+	}
+
+	private void runGenerator(WorldGenerator generator, World world, Random rand, int chunk_X, int chunk_Z, int chancesToSpawn, int minHeight, int maxHeight) {
+		if (minHeight < 0 || maxHeight > 256 || minHeight > maxHeight)
+			throw new IllegalArgumentException("Illegal Height Arguments for WorldGenerator");
+
+		int heightDiff = maxHeight - minHeight + 1;
+		for (int i = 0; i < chancesToSpawn; i++) {
+			int x = chunk_X * 16 + rand.nextInt(16);
+			int y = minHeight + rand.nextInt(heightDiff);
+			int z = chunk_Z * 16 + rand.nextInt(16);
+			generator.generate(world, rand, new BlockPos(x, y, z));
+		}
+	}
+
+}
