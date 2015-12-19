7c7c238cae4d2ce3e75e1ddbaaae7aabc0652a89
TechStack <techStackLp@gmail.com>
2015-12-19 09:42:34 -0500

    starting work on making the Oil rig only work on oceans needs more work

--- src/main/java/com/projectreddog/machinemod/item/machines/ItemOilRig.java
+++ src/main/java/com/projectreddog/machinemod/item/machines/ItemOilRig.java
@@ -6,10 +6,13 @@ import com.projectreddog.machinemod.item.ItemMachineMod;
 import com.projectreddog.machinemod.model.ModelTransportable;
 
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.BlockPos;
+import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.world.World;
+import net.minecraftforge.common.BiomeDictionary;
 
 public class ItemOilRig extends ItemMachineMod {
 
@@ -28,20 +31,28 @@ public class ItemOilRig extends ItemMachineMod {
 
 		if (!world.isRemote)// / only run on server
 		{
-			// LogHelper.info("Item used on loader!");
-			int x = pos.getX();
-			int y = pos.getY();
-			int z = pos.getZ();
 
-			EntityMachineModRideable entityOilRig = getEntityToSpawn(world);
-			entityOilRig.setPosition(x + .5d, y + 1.0d, z + .5d);
-			entityOilRig.prevPosX = x + .5d;
-			entityOilRig.prevPosY = y + 1.0d;
-			entityOilRig.prevPosZ = z + .5d;
-			result = world.spawnEntityInWorld(entityOilRig);
-			// LogHelper.info("Spawn entity resutl:" + result );
-			if (result && !player.capabilities.isCreativeMode) {
-				stack.stackSize--;
+			if (BiomeDictionary.isBiomeOfType(world.getBiomeGenForCoords(pos), BiomeDictionary.Type.OCEAN) && pos.getY()> 60 && world.isAirBlock(pos.up())  && world.getBlockState(pos).getBlock()==Blocks.water){
+				
+				// LogHelper.info("Item used on loader!");
+				int x = pos.getX();
+				int y = pos.getY();
+				int z = pos.getZ();
+
+				EntityMachineModRideable entityOilRig = getEntityToSpawn(world);
+				entityOilRig.setPosition(x + .5d, y + 1.0d, z + .5d);
+				entityOilRig.prevPosX = x + .5d;
+				entityOilRig.prevPosY = y + 1.0d;
+				entityOilRig.prevPosZ = z + .5d;
+				result = world.spawnEntityInWorld(entityOilRig);
+				// LogHelper.info("Spawn entity resutl:" + result );
+				if (result && !player.capabilities.isCreativeMode) {
+					stack.stackSize--;
+				}
+			}
+			else{
+				player.addChatComponentMessage(new ChatComponentText("You can only place an oil rig on an ocean's surface!"));
+
 			}
 		}
 		return result;
