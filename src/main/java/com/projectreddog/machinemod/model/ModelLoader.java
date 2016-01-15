558dbb0590561100264e8aaab0996bb98e4a87e0
TechStack <techStackLp@gmail.com>
2016-01-15 17:11:46 -0500

    Cleaned up code to simplify it for reuseablity across different machines

--- src/main/java/com/projectreddog/machinemod/model/ModelLoader.java
+++ src/main/java/com/projectreddog/machinemod/model/ModelLoader.java
@@ -1,6 +1,7 @@
 package com.projectreddog.machinemod.model;
 
 import java.io.IOException;
+import java.util.HashMap;
 
 import org.lwjgl.opengl.GL11;
 
@@ -21,18 +22,25 @@ public class ModelLoader extends ModelTransportable {
 	// fields
 	public OBJModel myOBJModel;
 	// private IFlexibleBakedModel fullModel;
-	private IFlexibleBakedModel modelBody;
-	private IFlexibleBakedModel modelArm2;
-	private IFlexibleBakedModel modelBucket;
+	// private IFlexibleBakedModel modelBody;
+	// private IFlexibleBakedModel modelArm2;
+	// private IFlexibleBakedModel modelBucket;
+	private String nameBody = "LoaderBody_Cube";
+	private String nameArm = "Arm2_Cube.002";
+	private String nameBucket = "Bucket_Cube.003";
+
+	private HashMap<String, IFlexibleBakedModel> modelParts;
 
 	public ModelLoader() {
 
 		try {
 			myOBJModel = (OBJModel) OBJLoader.instance.loadModel(new ResourceLocation(Reference.MOD_ID.toLowerCase(), "models/loader.obj"));
 			// fullModel = myOBJModel.bake(myOBJModel.getDefaultState(), Attributes.DEFAULT_BAKED_FORMAT, Reference.textureGetter);
-			modelBody = myOBJModel.bake(new OBJModel.OBJState(ImmutableList.of("LoaderBody_Cube"), false), Attributes.DEFAULT_BAKED_FORMAT, Reference.textureGetterFlipV);
-			modelArm2 = myOBJModel.bake(new OBJModel.OBJState(ImmutableList.of("Arm2_Cube.002"), false), Attributes.DEFAULT_BAKED_FORMAT, Reference.textureGetterFlipV);
-			modelBucket = myOBJModel.bake(new OBJModel.OBJState(ImmutableList.of("Bucket_Cube.003"), false), Attributes.DEFAULT_BAKED_FORMAT, Reference.textureGetterFlipV);
+			// modelBody =
+
+			modelParts.put(nameBody, myOBJModel.bake(new OBJModel.OBJState(ImmutableList.of(nameBody), false), Attributes.DEFAULT_BAKED_FORMAT, Reference.textureGetterFlipV));
+			modelParts.put(nameArm, myOBJModel.bake(new OBJModel.OBJState(ImmutableList.of(nameArm), false), Attributes.DEFAULT_BAKED_FORMAT, Reference.textureGetterFlipV));
+			modelParts.put(nameBucket, myOBJModel.bake(new OBJModel.OBJState(ImmutableList.of(nameBucket), false), Attributes.DEFAULT_BAKED_FORMAT, Reference.textureGetterFlipV));
 
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -43,14 +51,14 @@ public class ModelLoader extends ModelTransportable {
 		super.render(entity, f, f1, f2, f3, f4, f5);
 		// myModel.renderAll();
 
-		this.renderGroupObject("LoaderBody_Cube");
+		this.renderGroupObject(nameBody);
 		// GL11.glTranslatef(0f, -1.5f, -0.5f);
 		GL11.glTranslatef(0f, -2.25f, -1.05f);
 		if (entity != null) {
 
 			GL11.glRotatef(((EntityLoader) entity).Attribute1, 1, 0, 0);
 		}
-		this.renderGroupObject("Arm2_Cube.002");
+		this.renderGroupObject(nameArm);
 		// GL11.glTranslatef(0f, 1.2f, -1.2f);
 		GL11.glTranslatef(0f, 1.8f, -1.9f);
 		if (entity != null) {
@@ -58,23 +66,14 @@ public class ModelLoader extends ModelTransportable {
 				GL11.glRotatef((((EntityLoader) entity).Attribute1 + 30) * -2f, 1, 0, 0);
 			}
 		}
-		this.renderGroupObject("Bucket_Cube.003");
+		this.renderGroupObject(nameBucket);
 
 	}
 
 	public void renderGroupObject(String groupName) {
 		// myModel.renderPart(groupName);
 
-		if (groupName.equals("LoaderBody_Cube")) {
-			MachineModRenderHelper.renderBakedModel(modelBody);
-
-		} else if (groupName.equals("Arm2_Cube.002")) {
-			MachineModRenderHelper.renderBakedModel(modelArm2);
-
-		} else if (groupName.equals("Bucket_Cube.003")) {
-			MachineModRenderHelper.renderBakedModel(modelBucket);
-
-		}
+		MachineModRenderHelper.renderBakedModel(modelParts.get(groupName));
 
 	}
 
