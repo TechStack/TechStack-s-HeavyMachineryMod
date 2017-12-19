9e8a3f10eff730e0905cf03d7474e0989bab2614
TechStack <techStackLp@gmail.com>
2017-12-19 11:18:46 -0500

    Fix entity on farmland transition to land issue?

--- src/main/java/com/projectreddog/machinemod/entity/EntityMachineModRideable.java
+++ src/main/java/com/projectreddog/machinemod/entity/EntityMachineModRideable.java
@@ -368,10 +368,10 @@ public class EntityMachineModRideable extends Entity {
 		{
 
 			this.motionY = 0;
-			this.posY = (int) this.posY + 1;
-			if (this.world.getBlockState(new BlockPos(this.posX, this.posY - 1, this.posZ)).getBlock() == Blocks.FARMLAND ||
-					this.world.getBlockState(new BlockPos(this.posX, this.posY - 1, this.posZ)).getBlock() == Blocks.SOUL_SAND ) {
-				this.onGround = true;
+			this.posY = ((int) (this.posY + 1));
+			this.onGround = true;
+			if (this.world.getBlockState(new BlockPos(this.posX, this.posY - 1, this.posZ)).getBlock() == Blocks.FARMLAND || this.world.getBlockState(new BlockPos(this.posX, this.posY - 1, this.posZ)).getBlock() == Blocks.SOUL_SAND) {
+
 			}
 		}
 
