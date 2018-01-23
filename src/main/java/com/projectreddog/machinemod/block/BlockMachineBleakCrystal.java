cf8d9b9fc5b76efdb910504b2880537baceffad7
TechStack <techStackLp@gmail.com>
2018-01-23 11:43:08 -0500

    Bleak crystal block now renders

--- src/main/java/com/projectreddog/machinemod/block/BlockMachineBleakCrystal.java
+++ src/main/java/com/projectreddog/machinemod/block/BlockMachineBleakCrystal.java
@@ -3,6 +3,14 @@ package com.projectreddog.machinemod.block;
 import com.projectreddog.machinemod.reference.Reference;
 
 import net.minecraft.block.SoundType;
+import net.minecraft.block.state.BlockFaceShape;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.util.BlockRenderLayer;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class BlockMachineBleakCrystal extends BlockMachineMod {
 
@@ -17,4 +25,35 @@ public class BlockMachineBleakCrystal extends BlockMachineMod {
 		this.setSoundType(SoundType.GLASS);
 	}
 
+	/**
+	 * Used to determine ambient occlusion and culling when rebuilding chunks for render
+	 */
+	@Override
+	public boolean isOpaqueCube(IBlockState state) {
+		return false;
+	}
+
+	@Override
+
+	public boolean isFullCube(IBlockState state) {
+		return false;
+	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public BlockRenderLayer getBlockLayer() {
+		return BlockRenderLayer.TRANSLUCENT;
+	}
+
+	/**
+	 * Get the geometry of the queried face at the given position and state. This is used to decide whether things like buttons are allowed to be placed on the face, or how glass panes connect to the face, among other things.
+	 * <p>
+	 * Common values are {@code SOLID}, which is the default, and {@code UNDEFINED}, which represents something that does not fit the other descriptions and will generally cause other things not to connect to the face.
+	 * 
+	 * @return an approximation of the form of the given face
+	 */
+	@Override
+	public BlockFaceShape getBlockFaceShape(IBlockAccess worldIn, IBlockState state, BlockPos pos, EnumFacing face) {
+		return BlockFaceShape.UNDEFINED;
+	}
 }
