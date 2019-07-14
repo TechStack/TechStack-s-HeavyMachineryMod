2be96b171f8b9cdb01e437827da85831d29985c2
TechStack <TechStackLP@gmail.com>
2019-07-14 14:13:42 -0400

    import

--- src/main/java/com/projectreddog/machinemod/entity/ai/EntityAIFleeLight.java
+++ src/main/java/com/projectreddog/machinemod/entity/ai/EntityAIFleeLight.java
@@ -5,7 +5,6 @@ import java.util.Random;
 import javax.annotation.Nullable;
 
 import net.minecraft.entity.CreatureEntity;
-import net.minecraft.entity.CreatureEntity;
 import net.minecraft.entity.ai.EntityAIBase;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
