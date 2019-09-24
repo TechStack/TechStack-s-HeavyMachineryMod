4964099c2c6d2ce816d345204d635c07f2a2e8fb
TechStack <TechStackLP@gmail.com>
2019-09-24 11:02:49 -0400

    bleakglass resources

--- src/main/java/com/projectreddog/machinemod/block/BlockMachineBleakGlass.java
+++ src/main/java/com/projectreddog/machinemod/block/BlockMachineBleakGlass.java
@@ -1,12 +1,15 @@
 package com.projectreddog.machinemod.block;
 
-import java.util.Random;
-
+import com.projectreddog.machinemod.init.ModBlocks;
 import com.projectreddog.machinemod.reference.Reference;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.util.BlockRenderLayer;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
@@ -23,24 +26,37 @@ public class BlockMachineBleakGlass extends BlockMachineMod {
 		this.setSoundType(SoundType.GLASS);
 	}
 
-	/**
-	 * Returns the quantity of items to drop on block destruction.
-	 */
-	public int quantityDropped(Random random) {
-		return 0;
-	}
-
 	@SideOnly(Side.CLIENT)
 	public BlockRenderLayer getBlockLayer() {
-		return BlockRenderLayer.CUTOUT;
+		return BlockRenderLayer.TRANSLUCENT;
 	}
 
 	public boolean isFullCube(IBlockState state) {
 		return false;
 	}
 
-	protected boolean canSilkHarvest() {
-		return true;
+	/**
+	 * Used to determine ambient occlusion and culling when rebuilding chunks for render
+	 */
+	public boolean isOpaqueCube(IBlockState state) {
+		return false;
 	}
 
+	@SideOnly(Side.CLIENT)
+	public boolean shouldSideBeRendered(IBlockState blockState, IBlockAccess blockAccess, BlockPos pos, EnumFacing side) {
+		IBlockState iblockstate = blockAccess.getBlockState(pos.offset(side));
+		Block block = iblockstate.getBlock();
+
+		if (this == ModBlocks.machinebleakglass) {
+			if (blockState != iblockstate) {
+				return true;
+			}
+
+			if (block == this) {
+				return false;
+			}
+		}
+
+		return block == this ? false : super.shouldSideBeRendered(blockState, blockAccess, pos, side);
+	}
 }
